c2a5cb5ba739506637199acc02b4ab0b17ec9335
JieChongHai <3357991893@qq.com>
2022-10-31 17:59:37 +0800

    fix: demo code style

---
+++ src/examples/views/ArrayLayoutView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-formly-v3 ref="form" v-model="formData" :meta="meta" />
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import type VFormly from "@/Formly.vue";
+
+const form = ref<null | InstanceType<typeof VFormly>>(null);
+const formData = ref({});
+const meta = {
+  type: "object",
+  properties: {
+    product: {
+      type: "array",
+      title: "产品清单",
+      items: {
+        type: "object",
+        properties: {
+          name: {
+            type: "string",
+            title: "名称",
+          },
+          desc: { type: "string", title: "简介" },
+        },
+      },
+      ui: { grid: { arraySpan: 18 } },
+    },
+  },
+};
+</script>
