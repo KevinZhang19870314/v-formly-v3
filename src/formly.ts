6195a2668c10019e015d5243a3e46ca8ce6c3f25
kevin.zhang <kevinzhang19870314@gmail.com>
2022-11-03 14:35:35 +0800

    feat: revise element-ui

--- src/formly.ts
+++ src/formly.ts
@@ -1,8 +1,3 @@
-// import VFormly from "./Formly.vue";
-// import VWrapper from "@/components/Wrapper.vue";
-// import { BaseMeta } from "@/meta/base.meta";
-// import { registerFormComponent } from "@/ant-design-vue/register.factory";
-// import { FORM_VALUE_CHANGE } from "@/core/utils/consts";
 import type { App } from "vue";
 import mitt from "mitt";
 import aformly from "./ant-design-vue/a-formly";
@@ -15,7 +10,7 @@ const install = function (app: App, options: any) {
   } else if (options.lib === "element-ui") {
     // TODO
   } else {
-    throw new Error("Un support lib");
+    throw new Error("Unsupport lib");
   }
 
   // 传入自定义options
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,45 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { setupAntd } from "./ant-design-vue/examples/ant-design-vue";
-import * as antIcons from "@ant-design/icons-vue";
 import mitt from "mitt";
-
-import { setupStore } from "./ant-design-vue/examples/store";
-import { setupRouter } from "./ant-design-vue/examples/router";
-
 import VFormly from "./formly";
-import { registerFormComponent } from "./ant-design-vue/a-formly";
-import VPassword from "./ant-design-vue/examples/components/password/Password.vue";
-import VChkInput from "./ant-design-vue/examples/components/chk-input/ChkInput.vue";
-
-// import "./assets/main.css";
 import "@/style/index.less";
 
+// ant-design-vue import
+import { setupAntd } from "@/ant-design-vue/examples/ant-design-vue";
+import * as antIcons from "@ant-design/icons-vue";
+import { setupStore } from "@/ant-design-vue/examples/store";
+import { setupRouter } from "@/ant-design-vue/examples/router";
+import { registerFormComponent } from "@/ant-design-vue/a-formly";
+import VPassword from "@/ant-design-vue/examples/components/password/Password.vue";
+import VChkInput from "@/ant-design-vue/examples/components/chk-input/ChkInput.vue";
+
 const app = createApp(App);
-
 app.config.globalProperties.emitter = mitt();
-setupAntd(app);
-Object.keys(antIcons).forEach((key) => {
-  app.component(key, (antIcons as any)[key]);
-});
-app.config.globalProperties.$antIcons = antIcons;
-setupStore(app);
-setupRouter(app);
+const lib = "ant-design-vue";
 
-app.use(VFormly, {
-  lib: "",
-  ui: {
-    errors: {
-      required: "必填项",
+// ant-design-vue
+if (lib === "ant-design-vue") {
+  setupAntd(app);
+  Object.keys(antIcons).forEach((key) => {
+    app.component(key, (antIcons as any)[key]);
+  });
+  app.config.globalProperties.$antIcons = antIcons;
+  setupStore(app);
+  setupRouter(app);
+
+  app.use(VFormly, {
+    lib: lib,
+    ui: {
+      errors: {
+        required: "必填项",
+      },
     },
-  },
-});
+  });
 
-registerFormComponent(app, "v-password", VPassword);
-registerFormComponent(app, "v-chkinput", VChkInput);
+  registerFormComponent(app, "v-password", VPassword);
+  registerFormComponent(app, "v-chkinput", VChkInput);
+} else if (lib === "element-ui") {
+  // TODO: element-ui
+}
 
 app.mount("#app");
